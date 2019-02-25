96ca5f583183d1518bcc3afbe37f7f1a52783c7f
retina <retina@Joabe.local>
2019-02-25 09:58:43 -0300

    Implementing loadUrl

--- app/src/main/java/com/example/webviewsample/MainActivity.java
+++ app/src/main/java/com/example/webviewsample/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.webviewsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edtUrl;
+    private Button btnGo;
+    private WebView webViewSite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtUrl = findViewById(R.id.edtUrl);
+        btnGo = findViewById(R.id.btnGo);
+        webViewSite = findViewById(R.id.webViewSite);
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String address = "http://" + edtUrl.getText().toString();
+                webViewSite.loadUrl(address);
+            }
+        });
     }
 }
